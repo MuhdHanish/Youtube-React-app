a3c13f03cbda7f7a48a3e40868a7391190a5b870
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-25 20:15:02 +0530

    making dynamic channel card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import {Box,CardContent,CardMedia,Typography} from "@mui/material"
+import { CheckCircle } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = () => {
+
+
+const ChannelCard = ({channelDetail}) => {
   return (
-    <div>ChannelCard</div>
+    <Box sx={{
+     boxShadow:'none',
+    borderRadius:'20px'
+    }}>
+     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+     <CardContent sx={{display:'flex',flexDirection:'column',justifyContent:'center',texAlign:'center',color:'#fff'}}>
+      <CardMedia
+      image={channelDetail?.snippet?.thumbnails?.high?.url||demoProfilePicture}
+      alt={channelDetail?.snippet?.title}
+      sx={{borderRadius:'50%',height:'180px',width:'180px',md:2,border:'1px solid #e3e3e3'}}
+      />
+      <Typography variant='h6'>{channelDetail?.snippet?.title}
+      <CheckCircle sx={{fontSize:14,color:'gray',ml:"5px"}}/>
+      </Typography>
+      {channelDetail?.statistics?.subscriberCount&&(
+       <Typography>
+        {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()}Subscribers
+       </Typography>
+      )}
+     </CardContent>
+     </Link>
+    </Box>
   )
 }
 

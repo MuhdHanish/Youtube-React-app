ed005f4baba106ba1cbd3428f59fa8b74e42cfa0
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-25 19:48:46 +0530

    there is a small issue so just fixed but some code lost in that issue i re-write it

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,13 +7,12 @@ import Videos from "./Videos";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
-
   const [selectedCategory, setSelectedCategory] = useState("New");
-  const [videos,setVideos] = useState([])
-  
+  const [videos, setVideos] = useState([]);
+
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then(
-      (data) => setVideos(data.items)
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
     );
   }, [selectedCategory]);
 
@@ -39,20 +38,20 @@ const Feed = () => {
             Copyright 2023 MuhdHanish
           </Typography>
         </Box>
+        <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            mb={2}
+            sx={{
+              color: "#fff",
+            }}
+          >
+            {selectedCategory} <span style={{ color: "#F31503" }}>Videos</span>
+          </Typography>
+          <Videos videos={videos} />
+        </Box>
       </Stack>
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography
-          variant="h4"
-          fontWeight="bold"
-          mb={2}
-          sx={{
-            color: "#fff",
-          }}
-        >
-          {selectedCategory} <span style={{ color: "#F31503" }}>Videos</span>
-        </Typography>
-        <Videos videos={videos} />
-      </Box>
     </>
   );
 };
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -22,8 +22,8 @@ const SideBar = ({selectedCategory,setSelectedCategory}) => {
           }}
           key={category.name}
         >
-          <span style={{color:category.name===selectedCategory?'white':'red'}}>{category.icon}</span>
-          <span>{category.name}</span>
+          <span style={{color:category.name===selectedCategory?'white':'red',marginRight:'15px'}}>{category.icon}</span>
+          <span style={{opacity:category.name===selectedCategory?'1':'0.8'}}>{category.name}</span>
         </button>
       ))}
     </Stack>

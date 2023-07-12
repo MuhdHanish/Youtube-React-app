22b01c023eade81730e42f02df21e4cbd7f18fc9
MuhdHanish <muhammedhanish11@gmail.com>
2023-07-12 20:44:10 +0530

    a small bug fixed in the searching

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -30,7 +30,7 @@ const SearchBar = () => {
       value={searchTerm}
       onChange={(e)=>{setSearchTerm(e.target.value)}}
       />
-      <IconButton type='button' sx={{p:'10px',color:'red'}}>
+      <IconButton type='submit' sx={{p:'10px',color:'red'}}>
        <Search/>
       </IconButton>
     </Paper>
